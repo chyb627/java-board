08723bbd0f271d9ee44d5f497cfd0222ce3982ab
cha <chyb627@naver.com>
2024-10-16 16:14:49 +0900

    추가: first commit

---
+++ src/test/java/com/youngbin/javaboard/JavaBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youngbin.javaboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
